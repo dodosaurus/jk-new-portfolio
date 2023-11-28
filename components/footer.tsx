8bf0d472a68406d50937cbcd9db32ec9f13a0c47
dodosaurus <kovac.jozef25@gmail.com>
2023-11-28 17:05:17 +0100

    adding ByteGrad mention, my CV

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500 ">
       <small className="mb-2 block text-xs">
-        &copy; {new Date().getFullYear()} Jozef. All rights reserved.
+        &copy; {new Date().getFullYear()} Jozef. All rights reserved. Built with help of <a className="font-semibold" href="https://www.youtube.com/@ByteGrad">@ByteGrad</a>.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
