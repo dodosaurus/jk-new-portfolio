3ee1fdbfc919905dbca7b8547abe34fdd155b529
Jozef Kovac <jozef.kovac@hotovo.com>
2025-11-18 14:18:14 +0100

    removing CV button

--- components/intro.tsx
+++ components/intro.tsx
@@ -108,13 +108,13 @@ export default function Intro() {
         >
           Contact me <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a
+        {/* <a
           href="/CV.pdf"
           download
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
         >
           Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+        </a> */}
 
         <a
           href="https://www.linkedin.com/in/kovacjozef"
