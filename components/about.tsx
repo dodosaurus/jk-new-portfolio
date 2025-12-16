6492836b4f0c2a12c8772d2f948d0c085c73ba31
dodosaurus <kovac.jozef25@gmail.com>
2025-12-16 07:13:08 +0100

    adjusting texts

--- components/about.tsx
+++ components/about.tsx
@@ -20,26 +20,17 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       
       <p className="mb-3">
-        I graduated from <span className="font-medium">Pharmacy</span> and worked in retail pharmacy and <span className="italic">immunological research</span>, but retail wasn't my cup of tea due to my hunger for <span className="underline">more creative work</span>. In 2018, I transitioned to <span className="font-medium">software development</span>.
+        I graduated from <span className="font-medium">Pharmacy</span> and worked in both retail pharmacy and <span className="italic">immunological research</span>. However, retail work didn't satisfy my need for <span className="underline">creative problem-solving</span>. In 2018, I made the transition to <span className="font-medium">software development</span>.
       </p>
 
       <p className="mb-3">
-        After learning basics with Java, I switched to{" "}
-        <span className="underline">JavaScript</span> and through making dozens
-        of little projects, I fell in love with coding. I got my first job as{" "}
-        <span className="font-medium">Test Automation Engineer</span> on a great project full
-        of skillful people. I learned so much, especially about testing. After 4
-        years of experience with tools like Micro Focus UFT, ALM, Postman,
-        SoapUI, and most of all with <span className="italic">Cypress</span>, I
-        decided to move on. Currently, I am happily working in a similar position, but
-        in different setting and tools (like Robot Framework, NodeJS, ReadyAPI).
+        After learning the basics with Java, I switched to{" "}
+        <span className="underline">JavaScript</span> and fell in love with coding through building dozens of small projects. I landed my first role as a{" "}
+        <span className="font-medium">Test Automation Engineer</span> on a greenfield project with a team of skilled developers and testers. Over 4 years, I gained experience with tools like Micro Focus UFT, Postman, SoapUI, and especially <span className="italic">Cypress</span>. Currently, I work in a similar position using a different tech stack, including Robot Framework, NodeJS, and ReadyAPI.
       </p>
 
       <p className="mb-3">
-        <span className="italic">When I am not developing test automation</span>
-        , I enjoy working on personal projects. Coding gives me{" "}
-        <span className="underline">joy and sense of creative fulfillment</span>
-        . I am really thankful that I can do a job like this for a living 👌
+        When I'm not developing test automation, I enjoy working on personal projects. Coding gives me <span className="underline">joy and a sense of creative fulfillment</span>, and I'm grateful to do work that I'm passionate about.
       </p>
     </motion.section>
   );
